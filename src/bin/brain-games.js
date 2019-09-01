b232d60ecec638f10f5f4cb419b280accdfb3064
Konstantin Lukyanenok <loukianen@gmail.com>
2019-09-01 19:11:59 +0300

    step3

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node 
+
+import userName from './index.js';
 console.log('Welcome to the Brain Games!');
+console.log();
+console.log(`Hello, ${userName}!`);
---
+++ src/bin/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+const userName = readlineSync.question('May I have your name? ');
+
+export default userName;
